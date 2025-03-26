127665c2871d1ca5e5f3e5c7c84ccf89d9ece175
Muhammad Ranju <mdranju23@gmail.com>
2025-03-26 20:25:30 +0600

    refactor ContentComponent layout for improved alignment and readability of rating display

--- src/components/ContentComponent/ContentComponent.jsx
+++ src/components/ContentComponent/ContentComponent.jsx
@@ -34,10 +34,9 @@ const CardContent = ({
       </div>
 
       {/* Rating Component */}
-      <div className="flex items-center mt-2">
-        <span className="text-gray-600"></span>
+      <div className="flex items-center text-center mt-2">
         <StarRating rating={rating} />
-        <span className="text-gray-600 ml-2 font-semibold">{rating}</span>
+        <span className="text-gray-600 ml-2 mt-1 font-semibold">{rating}</span>
       </div>
 
       <p className="mb-3 font-normal text-gray-600">
